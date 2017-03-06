12689ef273291bdf2aeb60910d775f3cf51af002
DESKTOP-9MT21FL\xiaoYown <qzy09101018@sina.com>
2017-03-06 23:20:49 +0800

    Add_switch range

---
+++ vue/example/src/views/components/views/index.vue
@@ -0,0 +1,61 @@
+<template lang="jade">
+	.wrap-components
+		.ui-components-wrap
+			xy-switch(
+				:open="switchOpen",
+				@open-change="openChange"
+			)
+				p.inline {{ switchOpen }}
+			.ui-components-wrap
+			xy-range(
+				:percent="rangeNum",
+				@range-change="rangeChange"
+			)
+			p {{ rangeNum }}
+</template>
+
+<script>
+	export default {
+		components :{
+			'xy-switch': require('components/ui/switch.vue'),
+			'xy-range':  require('components/ui/range.vue'),
+		},
+		data(){
+			return {
+				switchOpen: true,
+				rangeNum: 0.5,
+			}
+		},
+		methods: {
+			openChange(bool){
+				this.switchOpen = bool;
+			},
+			rangeChange(range){
+				this.rangeNum = range;
+			}
+		}
+
+	}
+</script>
+
+<style lang="sass">
+	.wrap-components{
+		position: absolute;
+		top: 48px;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		
+		.inline{
+			vertical-align: middle;
+			margin-left: 10px;
+		}
+	}
+	.ui-components-wrap{
+		padding: 20px;
+
+		.its-range-wrap{
+			width: 200px;
+		}
+	}
+</style>
