91f39b75805f89a8dd81e53df24e17e2d905eeb6
DESKTOP-9MT21FL\xiaoYown <qzy09101018@sina.com>
2017-04-12 01:00:41 +0800

    Add_animation

---
+++ vue/example/src/views/demo/views/animation.vue
@@ -0,0 +1,56 @@
+<template lang="jade">
+    .wrap-demo-animation
+        .demo-animation
+            button(@click="dynamicAnimate(1)") 01
+            button(@click="dynamicAnimate(2)") 02
+            button(@click="dynamicAnimate(3)") 03
+            button(@click="moveIn") show
+            dynamic(:animation="dynamicType",:show="show")
+                .box
+</template>
+
+<script>
+    export default {
+        components: {
+            dynamic: require('components/animation/dynamic.vue')
+        },
+        data(){
+            return {
+                show: false,
+                dynamicType: 1,
+            }
+        },
+        methods: {
+            moveIn(){
+                this.show = !this.show;
+            },
+            dynamicAnimate(type){
+                this.dynamicType = type;
+            }
+        }
+    }
+</script>
+
+<style lang="sass" scoped>
+	.wrap-demo-animation{
+		position: absolute;
+		top: 48px;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		.demo-animation{
+            padding: 30px;
+            height: 200px;
+        }
+        .box{
+            width: 100px;
+            height: 100px;
+            background-color: #000;
+        }
+        button{
+            padding: 10px;
+            margin: 10px;
+            background-color: #56aaff;
+        }
+	}
+</style>
