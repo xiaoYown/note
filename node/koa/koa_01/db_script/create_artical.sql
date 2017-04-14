7a9d8eb0737876c59be06e465498551220a5a682
xiaoYown <511687372@qq.com>
2017-04-14 10:06:23 +0800

    add_editmd lib

---
+++ node/koa/koa_01/db_script/create_artical.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS article (
+	type_NO01 CHAR(20), 
+	type_NO02 CHAR(20), 
+	type_NO03 CHAR(20), 
+	type_name_NO01 CHAR(20), 
+	type_name_NO02 CHAR(20), 
+	type_name_NO03 CHAR(20), 
+	create_time DATETIME, 
+	update_time DATETIME, 
+	id VARCHAR(60),
+	introduction VARCHAR(200)
+)
