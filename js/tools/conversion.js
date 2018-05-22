9b14662c742ce1351c4a3007d2c70402c2ff4fef
DESKTOP-9MT21FL\xiaoYown <qzy09101018@sina.com>
2018-05-23 01:00:32 +0800

    Mod_单余数bug修复

--- js/tools/conversion.js
+++ js/tools/conversion.js
@@ -10,16 +10,15 @@ function AnyToTen (num) {
 function TenToAny (num, hex) {
   let result = ''
   let next = num
-  let maxHex = 25
+  let maxHex = 35
   let charCodeStart = 97
   let i = 0
-  let len = 10 + maxHex
   let store = {}
 
-  if (hex > maxHex + 10) { // 超过 36 字母能表示值
+  if (hex > maxHex) { // 超过 36 字母能表示值
     return console.log('hex over the max: ' + maxHex)
   } else {
-    for (; i <= len; i++) {
+    for (; i <= maxHex; i++) {
       if (i < 10) {
         store[i + ''] = i
       } else {
@@ -32,7 +31,7 @@ function TenToAny (num, hex) {
     result = store[next % hex + ''] + result
     next = Math.floor(next / hex)
   }
-  result = next + result
+  result = store[next] + result
 
   return result
 }
