8a9e0b4b1756ebc8f028e008aa645bd827a25828
DESKTOP-9MT21FL\xiaoYown <qzy09101018@sina.com>
2018-05-24 00:46:14 +0800

    feat 进制转换添加(仅正整数转换) 1) 任何进制转 10 进制 2) 任何进制转任何进制 3) 生成 char <-> 对应数值存储, char 仅取 a - z, 所以进制仅转换 35 进制以内

--- js/tools/conversion.js
+++ js/tools/conversion.js
@@ -1,37 +1,75 @@
-// 任何进制转任何进制
-function  Conversion (num, hexFrom, hexTo) {
-
-}
-// 任何进制转 10 进制
-function AnyToTen (num) {
-
-}
-// 10 进制转任何进制
-function TenToAny (num, hex) {
-  let result = ''
-  let next = num
+/**
+ * 任何进制转 10 进制
+ * @return {Object} num - 预存对应 char num 存储, 例: 97 <-> 'a', 122 <-> 'z'
+ */
+function HexCharStore () {
+  let i = 0
   let maxHex = 35
   let charCodeStart = 97
-  let i = 0
-  let store = {}
-
-  if (hex > maxHex) { // 超过 36 字母能表示值
-    return console.log('hex over the max: ' + maxHex)
-  } else {
-    for (; i <= maxHex; i++) {
-      if (i < 10) {
-        store[i + ''] = i
-      } else {
-        store[i + ''] = String.fromCharCode(charCodeStart - 10 + i)
-      }
+  let hexCharStore = {
+    toChar: {},
+    toNum: []
+  }
+  for (; i <= maxHex; i++) {
+    if (i < 10) {
+      hexCharStore.toChar[i + ''] = i
+      hexCharStore.toNum[i] = i
+    } else {
+      hexCharStore.toChar[i + ''] = String.fromCharCode(charCodeStart - 10 + i)
+      hexCharStore.toNum[String.fromCharCode(charCodeStart - 10 + i) + ''] = i
     }
   }
+  return hexCharStore
+}
 
-  while (next >= hex) {
-    result = store[next % hex + ''] + result
-    next = Math.floor(next / hex)
+/**
+ * 任何进制转 10 进制
+ * @param {Number, String} num - 对应进制数值
+ * @param {Number} hex - 进制
+ */
+function AnyToTen (num, hex) {
+  num = num + ''
+  let result = 0
+  let len = num.length
+  let i = 0
+  let store = HexCharStore().toNum
+
+  for (; i < len; i++) {
+    result += store[num[i]] * Math.pow(hex, len - i -1)
   }
-  result = store[next] + result
 
   return result
+}
+
+/**
+ * 10 进制转任何进制 
+ * @param {Number} num - 10进制数值
+ * @param {Number} hex - 输出到的进制
+ */
+function TenToAny (num, hex) {
+  let result = ''
+  let store = HexCharStore().toChar // 存储 1 ~ hex 对应数值表示
+
+  if (hex > 35) { // 超过 36 字母能表示值
+    return console.log('hex over the max: ' + maxHex)
+  }
+
+  while (num >= hex) {
+    result = store[num % hex + ''] + result
+    num = Math.floor(num / hex)
+  }
+  result = store[num] + result
+
+  return result
+}
+
+/**
+ * 任何转任何进制 
+ * @param {Number, String} num - 原始数值
+ * @param {Number} hexFrom - 输入的进制
+ * @param {Number} hexTo - 输出到的进制
+ */
+// 任何进制转任何进制
+function  Conversion (num, hexFrom, hexTo) {
+  return TenToAny(AnyToTen(num, hexFrom), hexTo)
 }
