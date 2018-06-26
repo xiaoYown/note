a81e2637026336e410c603e69715b9a8ece773ed
xiaoYown <qzy09101018@gmail.com>
2018-06-26 14:33:37 +0800

    Update vsCode.js

--- editor-setting/vscode/vsCode.js
+++ editor-setting/vscode/vsCode.js
@@ -4,7 +4,8 @@ const userSetting = {
     "editor.renderIndentGuides": true,
     "editor.mouseWheelZoom": true,
     "files.associations": {
-        "*.vue": "vue"
+        "*.vue": "vue",
+        "*.jsx": "javascriptreact"
     },
     "terminal.integrated.fontSize": 16,
     "typescript.check.tscVersion": false,
@@ -13,7 +14,6 @@ const userSetting = {
     "eslint.validate": [
         "javascript",
         "javascriptreact",
-        "html",
         "vue",
         "typescript",
         "typescriptreact"
@@ -21,18 +21,44 @@ const userSetting = {
 };
 // 快捷键设置
 const key = [
-    { "key": "ctrl+shift+down",      "command": "editor.action.moveLinesDownAction",
-                                     "when": "editorTextFocus && !editorReadonly" },
-    { "key": "ctrl+shift+up",        "command": "editor.action.moveLinesUpAction",
-                                     "when": "editorTextFocus && !editorReadonly" },
-    { "key": "ctrl+up",              "command": "editor.action.insertCursorAbove",
-                                     "when": "editorTextFocus" },
-    { "key": "ctrl+down",            "command": "editor.action.insertCursorBelow",
-                                     "when": "editorTextFocus" },
-    { "key": "ctrl+alt+down",        "command": "workbench.action.terminal.scrollDown",
-                                     "when": "terminalFocus" },
-    { "key": "ctrl+alt+up",          "command": "workbench.action.terminal.scrollUp",
-                                     "when": "terminalFocus" },
-    { "key": "ctrl+shift+/",         "command": "editor.action.blockComment",
-                                     "when": "editorTextFocus && !editorReadonly" }
+    {
+        "key": "ctrl+shift+down",
+        "command": "editor.action.moveLinesDownAction",
+        "when": "editorTextFocus && !editorReadonly"
+    },
+    {
+        "key": "ctrl+shift+up",
+        "command": "editor.action.moveLinesUpAction",
+        "when": "editorTextFocus && !editorReadonly"
+    },
+    {
+        "key": "ctrl+up",
+        "command": "editor.action.insertCursorAbove",
+        "when": "editorTextFocus"
+    },
+    {
+        "key": "ctrl+down",
+        "command": "editor.action.insertCursorBelow",
+        "when": "editorTextFocus"
+    },
+    {
+        "key": "ctrl+alt+down",
+        "command": "workbench.action.terminal.scrollDown",
+        "when": "terminalFocus"
+    },
+    {
+        "key": "ctrl+alt+up",
+        "command": "workbench.action.terminal.scrollUp",
+        "when": "terminalFocus"
+    },
+    {
+        "key": "ctrl+shift+/",
+        "command": "editor.action.blockComment",
+        "when": "editorTextFocus && !editorReadonly"
+    },
+    {
+        "key": "ctrl+d",
+        "command": "editor.action.deleteLines",
+        "when": "textInputFocus && !editorReadonly"
+    }
 ];
