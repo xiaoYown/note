a3b6fa75e30ca756dc2ffad207f10b5d40a7eab3
xiaoYown <qzy09101018@sina.com>
2021-12-24 00:07:37 +0800

    feat

--- node/ipc/client.js
+++ node/ipc/client.js
@@ -1,19 +1,60 @@
 const net = require("net");
-const path = require('path');
+const path = require("path");
 
-function createClient(ctlPath) {
+function sendMessage(ctlPath, content) {
   const client = net.createConnection({ path: ctlPath }, () => {
     // 'connect' 监听器。
     console.log("connected to server!");
-    client.write("world!\r\n");
-  });
-  client.on("data", (data) => {
-    console.log(data.toString());
+    client.write(content);
     client.end();
   });
-  client.on("end", () => {
-    console.log("disconnected from server");
+  // client.on("data", (data) => {
+  //   console.log(data.toString());
+  //   // client.end();
+  // });
+  // client.on("end", () => {
+  //   console.log("disconnected from server");
+  // });
+}
+
+function createClient(ctlPath) {
+  process.stdin.setEncoding("utf-8");
+
+  process.stdin.on("readable", () => {
+    let input;
+
+    while ((input = process.stdin.read()) !== null) {
+      const command = input.trim();
+
+      if (command === "exit") process.exit();
+
+      process.stdout.write(`input -> ${command}\n`);
+
+      sendMessage(
+        ctlPath,
+        JSON.stringify({
+          pid: process.pid,
+          content: command,
+        })
+      );
+    }
   });
+
+  // 从标准输入开始读取，因此进程不会退出。
+  // process.stdin.resume();
+
+  // process.on("SIGINT", () => {
+  //   console.log("Received SIGINT.");
+  //   process.exit(0);
+  // });
+
+  // // 使用单个函数处理多个信号
+  // function handle(signal) {
+  //   console.log(`Received ${signal}`);
+  // }
+
+  // process.on("SIGINT", handle);
+  // process.on("SIGTERM", handle);
 }
 
 createClient(path.resolve("ctl.sock"));
--- node/ipc/server.js
+++ node/ipc/server.js
@@ -1,11 +1,11 @@
-const net = require('net');
-const path = require('path');
-const fs = require('fs');
+const net = require("net");
+const path = require("path");
+const fs = require("fs");
 
-const ipcPath = path.resolve('ctl.sock');
+const ipcPath = path.resolve("ctl.sock");
 
-const TEST_LINK_SUCCESS = 'TEST_LINK_SUCCESS';
-const TEST_LINK_FAILED = 'TEST_LINK_FAILED';
+const TEST_LINK_SUCCESS = "TEST_LINK_SUCCESS";
+const TEST_LINK_FAILED = "TEST_LINK_FAILED";
 
 const isFile = (name) => {
   try {
@@ -14,21 +14,21 @@ const isFile = (name) => {
   } catch (_error) {
     return false;
   }
-}
+};
 
 const removeFile = (name) => {
   fs.unlinkSync(name);
-}
+};
 
-function isServerActive () {
+function isServerActive() {
   return new Promise((resolve) => {
     try {
       const client = net
         .createConnection({ path: ipcPath }, () => {
           client.end();
-          resolve({ status: TEST_LINK_SUCCESS })
+          resolve({ status: TEST_LINK_SUCCESS });
         })
-        .on('error', error => {
+        .on("error", (error) => {
           resolve({
             status: TEST_LINK_FAILED,
             error,
@@ -43,46 +43,60 @@ function isServerActive () {
   });
 }
 
-function isSocketPathFileExist (name) {
+function isSocketPathFileExist(name) {
   return isFile(name);
 }
 
-function createServer (ctlPath) {
+function createServer(ctlPath) {
   isServerActive()
     .then(({ status }) => {
       if (status === TEST_LINK_SUCCESS) {
-        console.log('Link is exist.')
+        console.log("Link is exist.");
         return;
       }
       if (isSocketPathFileExist(ipcPath)) {
-        console.log('Delete link file.')
+        console.log("Delete link file.");
         removeFile(ipcPath);
       }
-      const server = net.createServer();
+      const server = net.createServer((connection) => {
+        console.log("client connected");
+        connection.on("end", () => {
+          console.log("client disconnected");
+        });
+        connection.on("data", (data) => {
+          console.log(JSON.stringify(JSON.parse(data.toString()), null, 2));
+        });
+        connection.pipe(connection);
+      });
+
+      server.on("data", (data) => {
+        console.log(data);
+      });
+
+      // server.on("connection", (e) => {
+      //   console.log(e);
+      // });
+
+      server.on("error", (e) => {
+        if (e.code === "EADDRINUSE") {
+          console.log("Address in use, retrying...");
+          setTimeout(() => {
+            server.close();
+          }, 1000);
+        }
+      });
+
       server.listen({
         path: ctlPath,
         exclusive: true,
         readableAll: true,
         writableAll: true,
       });
-    
-      server.on("data", (data) => {
-        console.log(data);
-      });
-    
-      server.on('error', (e) => {
-        if (e.code === 'EADDRINUSE') {
-          console.log('Address in use, retrying...');
-          setTimeout(() => {
-            server.close();
-          }, 1000);
-        }
-      });
     })
-    .catch(error => {
-      console.log('****** Error ******');
+    .catch((error) => {
+      console.log("****** Error ******");
       console.log(error);
-    })
+    });
 }
 
 createServer(ipcPath);
